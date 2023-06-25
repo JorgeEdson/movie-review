66111638fde2111885aa3f44e9fec260a525bab5
Jorge Edson <jorge.edson.bsi@gmail.com>
2023-06-25 16:51:21 -0300

    change name folder Maps

---
+++ MovieReview.DataBase/Mappings/ReviewMap.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MovieReview.Database.Maps.Base;
+using MovieReview.Core.Domain.Entities;
+
+namespace MovieReview.Core.Database.Maps
+{
+    public class ReviewMap : BaseEntityMap<Review>
+    {
+        public ReviewMap() : base("Reviews")
+        {
+        }
+
+        public override void Configure(EntityTypeBuilder<Review> builder)
+        {
+            base.Configure(builder);
+
+            builder.Property(x => x.Note).HasColumnName("Note").IsRequired();
+            builder.Property(x => x.Description).HasColumnName("Description").HasMaxLength(int.MaxValue).IsRequired();
+            builder.Property(x => x.UserId).HasColumnName("UserId").IsRequired();
+            builder.Property(x => x.TitleId).HasColumnName("TitleId").IsRequired();
+
+            builder.HasOne(x => x.User).WithMany(x => x.Reviews).HasForeignKey(x => x.UserId);
+            builder.HasOne(x => x.Title).WithMany(x => x.Reviews).HasForeignKey(x => x.TitleId);
+            
+        }
+    }
+}
---
+++ MovieReview.DataBase/Mappings/UserMap.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MovieReview.Core.Domain.Entities;
+using MovieReview.Database.Maps.Base;
+
+
+namespace MovieReview.Database.Maps
+{
+    public class UserMap : BaseEntityMap<User>
+    {
+        public UserMap() :base("Users")
+        {}
+
+        public override void Configure(EntityTypeBuilder<User> builder)
+        {
+            base.Configure(builder);
+
+            builder.Property(x => x.Name).HasColumnName("Name").HasMaxLength(100).IsRequired();
+            builder.Property(x => x.BirthDate).HasColumnName("BirthDate").IsRequired();
+            builder.Property(x => x.Email).HasColumnName("Email").IsRequired();
+            builder.Property(x => x.Password).HasColumnName("Password").IsRequired();
+            builder.Property(x => x.IsAdministrator).HasColumnName("IsAdministrator").IsRequired();
+        }
+    }
+}
